cf2ccab7b77d3be0cf85bd6f69dcd5927d5c3f8c
Arlly Cavalcanti <arllyepc@gmail.com>
2023-05-02 18:39:59 -0300

    versão 02-05-23 - viamais: planos

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,107 +1,141 @@
 //Comportamentos para o menu
 
 function toggleChevronIcon(menuItem) {
-	const chevronIcon = menuItem.querySelector('iconify-icon');
-	if (chevronIcon) {
-		chevronIcon.classList.toggle('chevron-up');
-	}
+  const chevronIcon = menuItem.querySelector("iconify-icon");
+  if (chevronIcon) {
+    chevronIcon.classList.toggle("chevron-up");
+  }
 }
 
 function isMobile() {
-	// Você pode ajustar o valor 768 para corresponder à largura de tela que define um dispositivo móvel em seu design
-	return window.innerWidth <= 768;
+  // Você pode ajustar o valor 768 para corresponder à largura de tela que define um dispositivo móvel em seu design
+  return window.innerWidth <= 768;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-	const menuItems = document.querySelectorAll('.menu-item');
+document.addEventListener("DOMContentLoaded", () => {
+  const menuItems = document.querySelectorAll(".menu-item");
 
-	document.addEventListener('click', (event) => {
-		menuItems.forEach((menuItem) => {
-			const dropdownMenu = menuItem.querySelector('.dropdown-menu');
-			if (dropdownMenu && isMobile()) {
-				if (
-					!dropdownMenu.contains(event.target) &&
-					!menuItem.contains(event.target)
-				) {
-					dropdownMenu.style.display = 'none';
-				}
-			}
-		});
-	});
+  document.addEventListener("click", (event) => {
+    menuItems.forEach((menuItem) => {
+      const dropdownMenu = menuItem.querySelector(".dropdown-menu");
+      if (dropdownMenu && isMobile()) {
+        if (
+          !dropdownMenu.contains(event.target) &&
+          !menuItem.contains(event.target)
+        ) {
+          dropdownMenu.style.display = "none";
+        }
+      }
+    });
+  });
 
-	menuItems.forEach((menuItem) => {
-		if (!isMobile()) {
-			menuItem.addEventListener('mouseenter', () => {
-				const dropdownMenu = menuItem.querySelector('.dropdown-menu');
-				if (dropdownMenu) {
-					dropdownMenu.style.display = 'block';
-					toggleChevronIcon(menuItem);
-				}
-			});
+  menuItems.forEach((menuItem) => {
+    if (!isMobile()) {
+      menuItem.addEventListener("mouseenter", () => {
+        const dropdownMenu = menuItem.querySelector(".dropdown-menu");
+        if (dropdownMenu) {
+          dropdownMenu.style.display = "block";
+          toggleChevronIcon(menuItem);
+        }
+      });
 
-			menuItem.addEventListener('mouseleave', () => {
-				const dropdownMenu = menuItem.querySelector('.dropdown-menu');
-				if (dropdownMenu) {
-					dropdownMenu.style.display = 'none';
-					toggleChevronIcon(menuItem);
-				}
-			});
-		}
+      menuItem.addEventListener("mouseleave", () => {
+        const dropdownMenu = menuItem.querySelector(".dropdown-menu");
+        if (dropdownMenu) {
+          dropdownMenu.style.display = "none";
+          toggleChevronIcon(menuItem);
+        }
+      });
+    }
 
-		menuItem.addEventListener('click', (event) => {
-			if (isMobile()) {
-				event.preventDefault(); // Adicionado para evitar que o evento de clique seja propagado para elementos de âncora
+    menuItem.addEventListener("click", (event) => {
+      if (isMobile()) {
+        event.preventDefault(); // Adicionado para evitar que o evento de clique seja propagado para elementos de âncora
 
-				const dropdownMenu = menuItem.querySelector('.dropdown-menu');
-				if (dropdownMenu) {
-					if (dropdownMenu.style.display === 'block') {
-						dropdownMenu.style.display = 'none';
-					} else {
-						dropdownMenu.style.display = 'block';
-					}
-					toggleChevronIcon(menuItem);
-				}
-				// Previne a propagação do evento de clique para elementos pais
-				event.stopPropagation();
-			}
-		});
-	});
+        const dropdownMenu = menuItem.querySelector(".dropdown-menu");
+        if (dropdownMenu) {
+          if (dropdownMenu.style.display === "block") {
+            dropdownMenu.style.display = "none";
+          } else {
+            dropdownMenu.style.display = "block";
+          }
+          toggleChevronIcon(menuItem);
+        }
+        // Previne a propagação do evento de clique para elementos pais
+        event.stopPropagation();
+      }
+    });
+  });
 });
 
 //menu mobile
 
-const menuBtn = document.querySelector('.menu-btn');
-const navbarMobile = document.querySelector('.nav-mobile__nav');
+const menuBtn = document.querySelector(".menu-btn");
+const navbarMobile = document.querySelector(".nav-mobile__nav");
 
 let menuOpen = false;
 
-menuBtn.addEventListener('click', () => {
-	if (!menuOpen) {
-		menuBtn.classList.add('open');
-		navbarMobile.classList.add('active');
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    navbarMobile.classList.add("active");
 
-		menuOpen = true;
-	} else {
-		menuBtn.classList.remove('open');
-		navbarMobile.classList.remove('active');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    navbarMobile.classList.remove("active");
 
-		menuOpen = false;
-	}
+    menuOpen = false;
+  }
 });
 
 //accordion
 
 // selecionando todos os elementos de cabeçalho do accordion
-const accordionHeaders = document.querySelectorAll('.s-faq__accordion-header');
-accordionHeaders[0].nextElementSibling.classList.add('active');
+const accordionHeaders = document.querySelectorAll(".s-faq__accordion-header");
+accordionHeaders[0].nextElementSibling.classList.add("active");
 // adicionando um ouvinte de evento de clique a cada elemento de cabeçalho do accordion
 accordionHeaders.forEach((header) => {
-	header.addEventListener('click', () => {
-		header.classList.toggle('active');
-		// selecionando o elemento de conteúdo associado com o cabeçalho clicado
-		const content = header.nextElementSibling;
+  header.addEventListener("click", () => {
+    header.classList.toggle("active");
+    // selecionando o elemento de conteúdo associado com o cabeçalho clicado
+    const content = header.nextElementSibling;
 
-		// alternando a classe CSS "active" no elemento de conteúdo para controlar sua visibilidade
-		content.classList.toggle('active');
-	});
+    // alternando a classe CSS "active" no elemento de conteúdo para controlar sua visibilidade
+    content.classList.toggle("active");
+  });
+});
+
+var slide_plans = new Swiper(".slide-plans", {
+  // Default parameters
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  grabCursor: true,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
 });
