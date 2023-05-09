e24b407c39769a676d239f0438ed101dff86bf55
Arlly Cavalcanti <arllyepc@gmail.com>
2023-05-09 16:33:40 -0300

    viamais 29-05

--- js/all.js
+++ js/all.js
@@ -14,10 +14,10 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 
 //Comportamentos para o menu .
 function toggleChevronIcon(menuItem) {
-  var chevronIcon = menuItem.querySelector('iconify-icon');
+  var chevronIcon = menuItem.querySelector("iconify-icon");
 
   if (chevronIcon) {
-    chevronIcon.classList.toggle('chevron-up');
+    chevronIcon.classList.toggle("chevron-up");
   }
 }
 
@@ -26,50 +26,50 @@ function isMobile() {
   return window.innerWidth <= 768;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  var menuItems = document.querySelectorAll('.menu-item');
-  document.addEventListener('click', function (event) {
+document.addEventListener("DOMContentLoaded", function () {
+  var menuItems = document.querySelectorAll(".menu-item");
+  document.addEventListener("click", function (event) {
     menuItems.forEach(function (menuItem) {
-      var dropdownMenu = menuItem.querySelector('.dropdown-menu');
+      var dropdownMenu = menuItem.querySelector(".dropdown-menu");
 
       if (dropdownMenu && isMobile()) {
         if (!dropdownMenu.contains(event.target) && !menuItem.contains(event.target)) {
-          dropdownMenu.style.display = 'none';
+          dropdownMenu.style.display = "none";
         }
       }
     });
   });
   menuItems.forEach(function (menuItem) {
     if (!isMobile()) {
-      menuItem.addEventListener('mouseenter', function () {
-        var dropdownMenu = menuItem.querySelector('.dropdown-menu');
+      menuItem.addEventListener("mouseenter", function () {
+        var dropdownMenu = menuItem.querySelector(".dropdown-menu");
 
         if (dropdownMenu) {
-          dropdownMenu.style.display = 'block';
+          dropdownMenu.style.display = "block";
           toggleChevronIcon(menuItem);
         }
       });
-      menuItem.addEventListener('mouseleave', function () {
-        var dropdownMenu = menuItem.querySelector('.dropdown-menu');
+      menuItem.addEventListener("mouseleave", function () {
+        var dropdownMenu = menuItem.querySelector(".dropdown-menu");
 
         if (dropdownMenu) {
-          dropdownMenu.style.display = 'none';
+          dropdownMenu.style.display = "none";
           toggleChevronIcon(menuItem);
         }
       });
     }
 
-    menuItem.addEventListener('click', function (event) {
+    menuItem.addEventListener("click", function (event) {
       if (isMobile()) {
         event.preventDefault(); // Adicionado para evitar que o evento de clique seja propagado para elementos de âncora
 
-        var dropdownMenu = menuItem.querySelector('.dropdown-menu');
+        var dropdownMenu = menuItem.querySelector(".dropdown-menu");
 
         if (dropdownMenu) {
-          if (dropdownMenu.style.display === 'block') {
-            dropdownMenu.style.display = 'none';
+          if (dropdownMenu.style.display === "block") {
+            dropdownMenu.style.display = "none";
           } else {
-            dropdownMenu.style.display = 'block';
+            dropdownMenu.style.display = "block";
           }
 
           toggleChevronIcon(menuItem);
@@ -82,51 +82,51 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 }); //menu mobile
 
-var menuBtn = document.querySelector('.menu-btn');
-var navbarMobile = document.querySelector('.nav-mobile__nav');
+var menuBtn = document.querySelector(".menu-btn");
+var navbarMobile = document.querySelector(".nav-mobile__nav");
 var menuOpen = false;
-menuBtn.addEventListener('click', function () {
+menuBtn.addEventListener("click", function () {
   if (!menuOpen) {
-    menuBtn.classList.add('open');
-    navbarMobile.classList.add('active');
+    menuBtn.classList.add("open");
+    navbarMobile.classList.add("active");
     menuOpen = true;
   } else {
-    menuBtn.classList.remove('open');
-    navbarMobile.classList.remove('active');
+    menuBtn.classList.remove("open");
+    navbarMobile.classList.remove("active");
     menuOpen = false;
   }
 }); //accordion
 // selecionando todos os elementos de cabeçalho do accordion
 
-var accordionHeaders = document.querySelectorAll('.s-faq__accordion-header');
+var accordionHeaders = document.querySelectorAll(".s-faq__accordion-header");
 
 if (accordionHeaders.length > 0) {
   // adicionando a classe CSS "active" ao primeiro elemento de cabeçalho do accordion
-  accordionHeaders[0].nextElementSibling.classList.add('active');
+  accordionHeaders[0].nextElementSibling.classList.add("active");
 } // adicionando um ouvinte de evento de clique a cada elemento de cabeçalho do accordion
 
 
 accordionHeaders.forEach(function (header) {
-  header.addEventListener('click', function () {
-    header.classList.toggle('active'); // selecionando o elemento de conteúdo associado com o cabeçalho clicado
+  header.addEventListener("click", function () {
+    header.classList.toggle("active"); // selecionando o elemento de conteúdo associado com o cabeçalho clicado
 
     var content = header.nextElementSibling; // alternando a classe CSS "active" no elemento de conteúdo para controlar sua visibilidade
 
-    content.classList.toggle('active');
+    content.classList.toggle("active");
   });
 }); //SWIPER
 
-var slide_plans = new Swiper('.slide-plans', {
+var slide_plans = new Swiper(".slide-plans", {
   // Default parameters
   slidesPerView: 4,
   spaceBetween: 30,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
   },
   grabCursor: true,
   // Responsive breakpoints
@@ -143,8 +143,16 @@ var slide_plans = new Swiper('.slide-plans', {
     },
     // when window width is >= 640px
     640: {
-      slidesPerView: 4,
+      slidesPerView: 2,
       spaceBetween: 10
+    },
+    910: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    1210: {
+      slidesPerView: 4,
+      spaceBetween: 30
     }
   }
 });
@@ -153,47 +161,50 @@ var slide_plans = new Swiper('.slide-plans', {
 /* FILTROS SUPERIORES DE VELOCIDADE E PREÇO DA PÁGINA DE RESULTADOS */
 // Seleciona os elementos
 
-var mainBtn = document.querySelector('.results-filter-price__btn');
-var othersDiv = document.querySelector('.results-filter-price__others');
-var priceBtns = document.querySelectorAll('.others-group-price .others-group__btn');
-var velocityBtns = document.querySelectorAll('.others-group-velocity .others-group__btn'); // Adiciona a classe 'active' ao clicar em results-filter-price__btn
+var mainBtn = document.querySelector(".results-filter-price__btn");
+var othersDiv = document.querySelector(".results-filter-price__others");
+var priceBtns = document.querySelectorAll(".others-group-price .others-group__btn");
+var velocityBtns = document.querySelectorAll(".others-group-velocity .others-group__btn"); // Adiciona a classe 'active' ao clicar em results-filter-price__btn
 
-mainBtn.addEventListener('click', function () {
-  othersDiv.classList.toggle('active');
-}); // Função para lidar com a troca de botões ativos
+if (mainBtn) {
+  mainBtn.addEventListener("click", function () {
+    othersDiv.classList.toggle("active");
+  });
+} // Função para lidar com a troca de botões ativos
+
 
 function handleActiveBtn(targetBtn) {
   var allBtns = [].concat(_toConsumableArray(priceBtns), _toConsumableArray(velocityBtns));
   allBtns.forEach(function (btn) {
     if (btn === targetBtn) {
-      btn.classList.add('active');
+      btn.classList.add("active");
       mainBtn.textContent = btn.textContent;
     } else {
-      btn.classList.remove('active');
+      btn.classList.remove("active");
     }
   });
 } // Adiciona a classe 'active' ao clicar em um dos botões de others-group-price e others-group-velocity
 
 
 priceBtns.forEach(function (btn) {
-  btn.addEventListener('click', function () {
+  btn.addEventListener("click", function () {
     handleActiveBtn(btn);
   });
 });
 velocityBtns.forEach(function (btn) {
-  btn.addEventListener('click', function () {
+  btn.addEventListener("click", function () {
     handleActiveBtn(btn);
   });
 });
 /* Botão de 'ver detalhhes' no card de resultados de busca */
 
-document.querySelectorAll('.btn-details-desktop, .btn-details-mobile').forEach(function (btn) {
-  btn.addEventListener('click', function (event) {
-    var card = event.target.closest('.s-search-results__card');
-    var cardFooter = card.querySelector('.card__footer');
-    var showDetailsText = 'Ver detalhes';
-    var hideDetailsText = 'Esconder detalhes';
-    cardFooter.classList.toggle('active');
+document.querySelectorAll(".btn-details-desktop, .btn-details-mobile").forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+    var card = event.target.closest(".s-search-results__card");
+    var cardFooter = card.querySelector(".card__footer");
+    var showDetailsText = "Ver detalhes";
+    var hideDetailsText = "Esconder detalhes";
+    cardFooter.classList.toggle("active");
 
     if (event.target.innerHTML.trim() === showDetailsText) {
       event.target.innerHTML = hideDetailsText;
@@ -203,24 +214,97 @@ document.querySelectorAll('.btn-details-desktop, .btn-details-mobile').forEach(f
   });
 }); // Seleciona todos os elementos com a classe "aside-group__filter"
 
-var filterItems = document.querySelectorAll('.aside-group__filter'); // Adiciona um event listener a cada elemento
+var filterItems = document.querySelectorAll(".aside-group__filter"); // Adiciona um event listener a cada elemento
 
 filterItems.forEach(function (item) {
-  item.addEventListener('click', function () {
+  item.addEventListener("click", function () {
     // Verifica se o item clicado já tem a classe "active"
-    var isActive = item.classList.contains('active'); // Remove a classe "active" dos irmãos do elemento clicado
+    var isActive = item.classList.contains("active"); // Remove a classe "active" dos irmãos do elemento clicado
 
     item.parentNode.childNodes.forEach(function (sibling) {
       if (sibling.nodeType === Node.ELEMENT_NODE) {
-        sibling.classList.remove('active');
+        sibling.classList.remove("active");
       }
     }); // Se o elemento clicado já possui a classe "active", a remove
     // Caso contrário, adiciona a classe "active"
 
     if (isActive) {
-      item.classList.remove('active');
+      item.classList.remove("active");
     } else {
-      item.classList.add('active');
+      item.classList.add("active");
     }
   });
 });
+var btnSearch = document.querySelector("#btn-search-plans");
+var modalSearch = document.querySelector("#modal-searching-plan");
+btnSearch.addEventListener("click", function (event) {
+  event.preventDefault();
+  modalSearch.classList.add("active"); // Defina a URL para a qual você deseja redirecionar e o tempo de espera em milissegundos
+
+  var redirectUrl = "https://example.com";
+  var timeoutDuration = 5000; // 5000ms = 5s
+  // Redireciona para a URL após o tempo de espera
+
+  setTimeout(function () {
+    window.location.href = redirectUrl;
+  }, timeoutDuration);
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var showDetailsButtons = document.querySelectorAll(".show-details");
+  var modals = document.querySelectorAll(".card-plan__modal");
+  showDetailsButtons.forEach(function (button, index) {
+    button.setAttribute("data-card-index", index);
+    button.addEventListener("click", function (event) {
+      var cardIndex = event.target.getAttribute("data-card-index");
+      modals[cardIndex].classList.add("show");
+    });
+  });
+  modals.forEach(function (modal) {
+    var closeBtn = modal.querySelector(".close-btn");
+    closeBtn.addEventListener("click", function () {
+      modal.classList.remove("show");
+    }); // Fechar o modal ao clicar fora do conteúdo
+
+    modal.addEventListener("click", function (event) {
+      var modalContent = modal.querySelector(".modal-content");
+
+      if (!modalContent.contains(event.target)) {
+        modal.classList.remove("show");
+      }
+    });
+  });
+});
+/* document.addEventListener("DOMContentLoaded", () => {
+  const showDetailsButtons = document.querySelectorAll(".show-details");
+  const modal = document.querySelector("#planModal");
+  const modalContent = modal.querySelector(".modal-content");
+  const closeModalBtn = modal.querySelector(".close-btn");
+
+  showDetailsButtons.forEach((button, index) => {
+    button.setAttribute("data-card-index", index);
+
+    button.addEventListener("click", (event) => {
+      const cardIndex = event.target.getAttribute("data-card-index");
+      const card = document.querySelectorAll(".card-plan")[cardIndex];
+      const modalList = card.querySelector(".modal__list").cloneNode(true);
+
+      // Limpar o conteúdo do modal e preencher com o conteúdo do card clicado
+      modalContent.querySelector(".modal__list").innerHTML = "";
+      modalContent.querySelector(".modal__list").appendChild(modalList);
+
+      modal.classList.add("show");
+    });
+  });
+
+  closeModalBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+  });
+
+  // Fechar o modal ao clicar fora do conteúdo
+  modal.addEventListener("click", (event) => {
+    if (!modalContent.contains(event.target)) {
+      modal.classList.remove("show");
+    }
+  });
+});
+ */
